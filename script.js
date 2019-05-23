2caa50700687d432598705e54d4ac9742177470a
Roman Engler <romanengler@Romans-MBP.byod.zhdk.ch>
2019-05-23 13:56:06 +0200

    colour styling

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  var burger = $('#burger');
+  var navigation = $('#navigation');
+
+  function transformBurgerIcon() {
+    burger.toggleClass('is-active');
+  }
+
+  function showNavigation() {
+    navigation.toggleClass('is-open');
+  }
+
+  burger.click(function(event) {
+    transformBurgerIcon();
+    showNavigation();
+  });
+});
