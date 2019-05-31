42ba2564924ae83e0077efa41654ec22ccb28a75
Roman Engler <romanengler@Romans-MBP.byod.zhdk.ch>
2019-05-31 16:55:42 +0200

    beautify syntax

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 
   var $popup = $(".popup");
-  var $mySelectedItems = $(".grid-item_select");
+  var $mySelectedItems = $(".grid_item_select");
   console.log($mySelectedItems);
 
   $mySelectedItems.click(function() {
     console.log("click");
 
-    $(".popup").toggleClass("is-shown")
+    $(".popup").toggleClass("is_shown")
   });
 
 
-var $deleteButton = $(".popup_article_delete")
-$deleteButton.click(function(event) {
+  var $deleteButton = $(".popup_article_delete")
+  $deleteButton.click(function(event) {
     console.log("click");
     var $target = $(event.target);
     var $parentOfClickedButton = $target.closest(".popup_article");
@@ -25,18 +25,18 @@ $deleteButton.click(function(event) {
 
 
   var $popup = $(".popup");
-  var $mySelectedItems = $(".grid-item_select");
+  var $mySelectedItems = $(".grid_item_select");
   console.log($mySelectedItems);
 
   $mySelectedItems.hover(function() {
     console.log("hover");
 
-    $(".popup").toggleClass("is-shown-by-hover")
+    $(".popup").toggleClass("is_shown-by-hover")
   });
 
 
-var deleteButton = $(".popup_article_delete")
-deleteButton.hover(function(event) {
+  var deleteButton = $(".popup_article_delete")
+  deleteButton.hover(function(event) {
     console.log("hover");
     var $target = $(event.target);
     var $parentOfClickedButton = $target.closest(".popup_article");
