21c74cd00c890bfd0458b97d2b737f6bfa79c1f7
Roman Engler <romanengler@Romans-MBP.byod.zhdk.ch>
2019-05-31 14:04:03 +0200

    hover on popup and content alignment

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
   });
 
 
-var deleteButton = $(".popup_article_delete")
-deleteButton.click(function(event) {
+var $deleteButton = $(".popup_article_delete")
+$deleteButton.click(function(event) {
     console.log("click");
     var $target = $(event.target);
     var $parentOfClickedButton = $target.closest(".popup_article");
@@ -20,6 +20,31 @@ deleteButton.click(function(event) {
     console.log($parentOfClickedButton);
 
     $parentOfClickedButton.hide();
+  });
+
+
+
+  var $popup = $(".popup");
+  var $mySelectedItems = $(".grid-item_select");
+  console.log($mySelectedItems);
+
+  $mySelectedItems.hover(function() {
+    console.log("hover");
+
+    $(".popup").toggleClass("is-shown-by-hover")
+  });
+
+
+var deleteButton = $(".popup_article_delete")
+deleteButton.hover(function(event) {
+    console.log("hover");
+    var $target = $(event.target);
+    var $parentOfClickedButton = $target.closest(".popup_article");
+
+    console.log($parentOfClickedButton);
+
+
+
   });
 
 
