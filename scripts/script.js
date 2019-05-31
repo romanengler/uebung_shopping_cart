f3910370b18c3b40101c52424bf0164179034481
Roman Engler <romanengler@Romans-MBP.byod.zhdk.ch>
2019-05-31 18:16:07 +0200

    add to cart 1st article

--- scripts/script.js
+++ scripts/script.js
@@ -44,9 +44,27 @@ $(document).ready(function() {
 
     console.log($parentOfClickedButton);
 
-
-
   });
 
+
+
+
+  $('.js-button').click(function(event) {
+  $button = $(event.target);
+
+  // change the buttons text
+  $button.text('Added');
+
+  // get data attribute to revel correct item in cart
+  var itemName = $button.attr('data-item');
+  console.log('itemName: ', itemName);
+
+  // get the right item to reveal
+  var $itemToReveal = $('#' + itemName);
+  console.log('$itemToReveal: ', $itemToReveal);
+
+  $itemToReveal.removeClass('is-hidden');
+
+});
 
 });
